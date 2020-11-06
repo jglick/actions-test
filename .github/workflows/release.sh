70b02e2dc452eac9f750b318efc52248633618ce
Jesse Glick <jglick@cloudbees.com>
2020-11-06 15:50:21 -0500

    The release-drafter Action only works on push events, so we cannot use the publish option or include it in the release flow

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 version=$(git rev-list --count HEAD).$(git rev-parse --short=12 HEAD)
-echo "::set-output name=version::$version"
+alias curlu='curl -H "Authorization: Bearer $GITHUB_TOKEN"'
 # TODO would be more legible with jo:
-curl -H "Authorization: Bearer $GITHUB_TOKEN" -s -d '{"ref":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs
+$curlu -s -d '{"ref":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs
+release=$($curlu -s $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .id')
+$curlu -X PATCH -d '{"draft":"false","name":"'$version'","tag_name":"'$version'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases/$release
