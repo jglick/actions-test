d9bde2f5c0e52fe0a2ebd3eb487a1f1f8ffd6190
Jesse Glick <jglick@cloudbees.com>
2020-11-06 15:59:27 -0500

    Alias not var

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -3,6 +3,6 @@ set -euxo pipefail
 version=$(git rev-list --count HEAD).$(git rev-parse --short=12 HEAD)
 alias curlu='curl -H "Authorization: Bearer $GITHUB_TOKEN"'
 # TODO would be more legible with jo:
-$curlu -s -d '{"ref":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs
-release=$($curlu -s $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .id')
-$curlu -X PATCH -d '{"draft":"false","name":"'$version'","tag_name":"'$version'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases/$release
+curlu -s -d '{"ref":"refs/tags/'$version'","sha":"'$GITHUB_SHA'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/git/refs
+release=$(curlu -s $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases | jq -e -r '.[] | select(.draft == true and .name == "next") | .id')
+curlu -X PATCH -d '{"draft":"false","name":"'$version'","tag_name":"'$version'"}' $GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases/$release
